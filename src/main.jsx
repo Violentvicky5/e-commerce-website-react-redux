1febfa5ecc161ba10004db26da7d15f0efea822e
unknown <violentvicky5@gmail.com>
2025-11-10 14:37:56 +0530

    changed file path for store

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { Provider } from 'react-redux';
-import { Store } from './app/store';
+import { Store } from './app/Store.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
